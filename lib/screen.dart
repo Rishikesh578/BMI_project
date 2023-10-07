a54c21dcf61740ced183e6eede91e6dd7cc8f3ff
Rishikesh Raj <rishirajkumar2222@gmail.com>
2023-10-07 10:34:45 +0530

    Add Splash Screen

--- lib/screen.dart
+++ lib/screen.dart
@@ -15,38 +15,42 @@ class _Splash_screenState extends State<Splash_screen> {
   void initState() {
     super.initState();
 
-     Timer(Duration(seconds: 2), () {
+     Timer(Duration(seconds:3), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-        builder: (context) => MyHomePage(),
+        builder: (context) => MyApp(),
         )); 
      });
-
   }
-
-
-
-
-
-
-
-
-
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: Center(
-        child:
-          CircleAvatar(
-        backgroundImage: AssetImage('assets/bmi_image.png'),
-        radius: 130),
-      ),
-           
+     body: Column(
+       children: [
+           SizedBox(height: 28),
+         Center(
+            child:
+              CircleAvatar(
+            backgroundImage: AssetImage('assets/BMIAp.png'),
+            radius: 130),
+          ),
+
+           SizedBox(height:90),
+           ListTile(
+            title: Text('Designed By Rishikesh Raj',
+            style: TextStyle(fontStyle:FontStyle.italic),
+            textAlign: TextAlign.center,
+            ),
+           )
+       ],
+     ),
+
       
-    );
+       
+      
+     ) ;
+    
   }
 }
